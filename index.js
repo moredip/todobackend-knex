3ca8e5e20bfac15e7d872960b9f26be8fcc17ec7
Pete Hodgson <git@thepete.net>
2017-12-14 23:01:47 -0800

    add Procfile

--- index.js
+++ index.js
@@ -5,10 +5,11 @@ const PORT = process.env.PORT || 3000;
 
 // App
 const app = express();
+
 app.get('/', (req, res) => {
   res.send('Hello world\n');
 });
 
 app.listen(PORT, ()=> {
-  console.log(`server listening at port ${PORT}`);
+  console.log(`Listening on port ${PORT}`);
 });
