ca02d112a1c49e6d667271d707bd42b208246e72
Pete Hodgson <git@thepete.net>
2017-12-15 00:17:54 -0800

    move extension installation into migrations

--- db/migrations/20171214234217_create-todos-table.js
+++ db/migrations/20171214234217_create-todos-table.js
@@ -1,11 +1,13 @@
 // based on https://github.com/elnaz/todo-hapi-postgres/blob/master/db/migrations/20160919231029_create_todos.js
 exports.up = function(Knex, Promise){
-  return Knex.schema.createTable('todos', (table) => {
-    table.uuid('id').primary().defaultTo(Knex.raw("uuid_generate_v1mc()")).notNullable();
-    table.text('title').notNullable();
-    table.boolean('completed').defaultTo(false).notNullable();
-    table.integer('order');
-  });
+  return Knex.schema
+    .raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
+    .createTable('todos', (table) => {
+      table.uuid('id').primary().defaultTo(Knex.raw("uuid_generate_v1mc()")).notNullable();
+      table.text('title').notNullable();
+      table.boolean('completed').defaultTo(false).notNullable();
+      table.integer('order');
+    });
 }
 
 exports.down = function(){
